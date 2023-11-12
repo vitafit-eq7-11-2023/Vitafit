3aa6bddc068ed3e0881682a85eed141e80754a88
vitafit-eq7-11-2023 <vitafit.fo@gmail.com>
2023-11-11 22:30:36 -0500

    cambios-11/11/2023

--- modulos/conexion.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conexion = mysqli_connect("localhost","root","","vitafit") or die("Error en la conexion");
-?>
--- modulos/salir.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Inicio de session iniciada
-session_start();
-//Session eliminada
-session_destroy();
-//Redirigimos afuera 
-header("location: index.php");
-?>
